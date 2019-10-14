d3af967eb47bd91baffeae0eb64e1fecea4b1b77
devPinheiro <samuelpinheiro41@gmail.com>
2019-10-14 23:27:46 +0100

    landing page

--- src/client/pages/Home/index.js
+++ src/client/pages/Home/index.js
@@ -6,6 +6,12 @@ export default class Home extends Component {
     return (
       <div>
         <Hero />
+        <div className="blog section w-full max-w-screen-xl relative mx-auto px-6  sm:pr-20 sm:pl-20">
+          <div className="border-gray-800 ">
+              <h4>We build awesome stuffs too</h4>
+              <p>we are always adventurous and that drive us to build cool product</p>
+          </div>
+        </div>
       </div>
     )
   }
