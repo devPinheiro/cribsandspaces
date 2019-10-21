a761808e5ae9b20fc9d8174ea1d8eb255d3b1d03
devPinheiro <samuelpinheiro41@gmail.com>
2019-10-21 17:54:18 +0100

    added blog route

--- src/client/App.js
+++ src/client/App.js
@@ -1,15 +1,33 @@
+
 import React from 'react';
 import Home from '../client/pages/Home';
-import { BrowserRouter as Router } from 'react-router-dom';
+import Blog from '../client/pages/Blogs'
+import { BrowserRouter as Router, Route, withRouter } from 'react-router-dom';
 import Footer from './components/Footer';
+import Header from './components/Header';
+
+
+const Main = withRouter(({ location }) => {
+  return (
+    <>
+     {["/blog"].includes(
+        location.pathname
+      ) ||
+        (["/"].includes(location.pathname) && <Header />)}
+    <Route path="/" exact component={Home} />
+    <Route path="/blog" exact component={Blog} />
+   <Footer />
+   </>
+  )
+})
+
 
 const App = () => {
   return (
     <Router>
-        <Home />
-        <Footer />
+      <Main />
     </Router>
   )
-}
+};
 
 export default App
--- src/client/pages/Blogs/index.js
+++ src/client/pages/Blogs/index.js
@@ -1,51 +1,11 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react'
 
-class index extends Component {
-  constructor(props) {
-    super(props);
-
-  }
-
-  componentWillMount() {
-
-  }
-
-  componentDidMount() {
-
-  }
-
-  componentWillReceiveProps(nextProps) {
-
-  }
-
-  shouldComponentUpdate(nextProps, nextState) {
-
-  }
-
-  componentWillUpdate(nextProps, nextState) {
-
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-
-  }
-
-  componentWillUnmount() {
-
-  }
-
-  render() {
-    return (
-      <div>
-
-      </div>
-    );
-  }
+const Blog = () => {
+  return (
+    <div>
+        <h1>A list of blog posts would come here + infinite scroll</h1>
+    </div>
+  )
 }
 
-index.propTypes = {
-
-};
-
-export default index;
+export default Blog
