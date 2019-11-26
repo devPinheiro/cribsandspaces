cb5c381389f32f63868cd37995f2e5f3dfac3b84
devPinheiro <samuelpinheiro41@gmail.com>
2019-11-26 15:34:42 +0100

    fetch data with graphql

--- src/client/components/Card/index.js
+++ src/client/components/Card/index.js
@@ -1,30 +1,33 @@
 import React from 'react';
-import Proptypes from 'prop-types'
-import Image from '../Image';
+import PropTypes from 'prop-types';
 
-const Card = ({ image, title, description, tags, ...rest}) => {
+const Card = ({ featured_image, title, description, tags, ...rest}) => {
   return (
 
-      <div className="w-1/3 m-2 rounded overflow-hidden shadow-lg">
-            <img className="w-full" src="https://res.cloudinary.com/appnet/image/upload/v1571059042/cns/cribs_logo.jpg" alt="Sunset in the mountains" />
+      < div className = "max-w-sm  m-2 rounded overflow-hidden shadow-lg" >
+            <img className="w-full"
+            src={
+              featured_image[0].url ? featured_image[0].url : "https://res.cloudinary.com/appnet/image/upload/v1571059042/cns/cribs_logo.jpg"
+            }
+            alt = "Sunset in the mountains" />
             <div className="px-6 py-4">
               <div className="font-bold text-xl mb-2">{title}</div>
+             
               <p className="text-gray-700 text-base">
                 {description}
               </p>
             </div>
             <div className="px-6 py-4">
-            {
+            {/* {
               tags.map( tag => {
                 return (
                   <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">{tag}</span>
                 )
               })
-            }
-              
-             
+            } */}
+                       
             </div>
-            </div>
+          </div>
 
   )
 }
@@ -34,7 +37,7 @@ Card.propTypes = {
   description: PropTypes.string,
   tag: PropTypes.array,
   image: PropTypes.string,
-  rest: Proptypes.any
+  rest: PropTypes.any
 };
 
 export default Card
