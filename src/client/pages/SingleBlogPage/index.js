3003b55a2798724aa39a6827476cb0db7552bac4
devPinheiro <samuelpinheiro41@gmail.com>
2019-12-30 23:26:16 +0100

    single blog page

--- src/client/pages/SingleBlogPage/index.js
+++ src/client/pages/SingleBlogPage/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Card from '../../components'
 
 const SingleBlogPage = () => {
   return (
     <div>
       <h1>Single Blog page should be here</h1>
+      <Card />
     </div>
   )
 }
