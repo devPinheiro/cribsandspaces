86cdafd2f8851e27d7d1d69f9416f6e4264bd0f6
devPinheiro <samuelpinheiro41@gmail.com>
2020-02-02 03:37:51 +0100

    add dist

--- src/client/App.js
+++ src/client/App.js
@@ -1,79 +1,52 @@
 import React from 'react';
-import Home from '../client/pages/Home';
-import Blog from '../client/pages/Blogs';
-import SingleBlogPage from "../client/pages/SingleBlogPage";
-import {
-  BrowserRouter as Router,
-  Route,
-  withRouter
-} from 'react-router-dom';
+import { BrowserRouter as Router, Route, withRouter } from 'react-router-dom';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import Home from './pages/Home';
+import Blog from './pages/Blogs';
+import SingleBlogPage from './pages/SingleBlogPage';
 import Footer from './components/Footer';
 import Header from './components/Header';
-import {
-  ApolloProvider
-} from 'react-apollo';
-import {
-  ApolloClient
-} from 'apollo-client';
-import {
-  createHttpLink
-} from 'apollo-link-http';
-import {
-  InMemoryCache
-} from 'apollo-cache-inmemory';
 
 // create http link for remote data fetching
 const httpLink = createHttpLink({
-  uri: 'https://cns-app.herokuapp.com/graphql'
-})
+  uri: 'https://cns-app.herokuapp.com/graphql',
+});
 
 // Apollo Client setup
 const client = new ApolloClient({
   link: httpLink,
-  cache: new InMemoryCache()
-})
+  cache: new InMemoryCache(),
+});
 
+const Main = withRouter(({ location }) => {
+  return (
+    <>
+      {['/blog', '/posts'].includes(location.pathname) && (
+        <div className=" mx-auto px-6  sm:pr-20 sm:pl-20">
+          {' '}
+          <Header />{' '}
+        </div>
+      )}
+      <Route path="/" exact component={Home} />
+      <Route path="/blog" exact component={Blog} />
+      <Route path="/posts/:slug" exact component={SingleBlogPage} />
 
+      <Footer />
+    </>
+  );
+});
 
-const Main = withRouter(({
-      location
-    }) => {
-      return ( <> 
-        {
-        (["/blog", "/posts"].includes(location.pathname) && < div className = " mx-auto px-6  sm:pr-20 sm:pl-20" > < Header /> </div>)} 
-        <Route path = "/"
-          exact component = {
-            Home
-          }
-          /> 
-          <Route path = "/blog"
-          exact component = {
-            Blog
-          }
-          /> 
-          <Route path = "/posts/:slug"
-          exact component = {
-            SingleBlogPage
-          }
-          /> 
-         
-          <Footer />
-          </>
-        )
-      })
+const App = () => {
+  return (
+    <ApolloProvider client={client}>
+      <Router>
+        <Main />
+      </Router>
+    </ApolloProvider>
+  );
+};
 
-
-
-      const App = () => {
-        return ( 
-        <ApolloProvider client = {
-            client
-          } >
-          <Router>
-          <Main />
-          </Router> 
-          </ApolloProvider>
-        )
-      };
-
-      export default App
+export default App;
